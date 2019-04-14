1b28d03d14b927025d190165a149d5bd4a8449e7
Daniel Santos <dfsantosbu@unal.edu.co>
2019-04-13 23:27:37 -0500

    Make work code, not optimized.

--- code/code.cpp
+++ code/code.cpp
@@ -3,7 +3,8 @@
  *    created: 04.2019       
 **/
 #include<bits/stdc++.h>
-#define debug( x ) cout << #x << " = " << x ;
+#define PB push_back
+#define debug1( x ) cout << #x << " = " << x  <<endl;
 #define debug2( x, y) cout <<#x << " = " << x << " , " <<#y << " = " << y <<endl;
 #define rep( a,b, c) for(__typeof(b) a = b; a <(c); a++)
 
@@ -49,20 +50,39 @@ struct kdtree{
   int n;
   kdtree(int _n ): pts( _n ), n( _n ){
     rep( i, 0, n) rep(j, 0, dim) cin >> pts[ i ].x[ j ];
+    sort( all(pts), cmp(0));
   }
   void show(){
     for( pt a: pts) a.show();
   }
-  node* build(int l, int r, int d){
+  node* build(vector< pt> &A, int l, int r, int d){
+    /* for( pt c: A) c.show(); cout <<endl; */
     if( l >= r) {
-      return new node( d, pts[l] );
+      return new node( d, A[0] );
     }
-    sort( pts.begin()+l, pts.begin()+r, cmp( d ));
-    int mid = ( l + r )/2;
-    node *root = new node(d, pts[mid]);
     int nd = (d+1)%dim;
-    root->l = build( l, mid, nd);
-    root->r = build( mid+1, r, nd);
+    int mid = ( l + r )/2;
+    node *root;
+    if( A.size()==2){
+      if( A[0].x[nd] > A[1].x[nd] ){
+        root = new node(nd, A[0]);
+        root->l = new node( nd, A[1]);
+      }else{
+        root = new node(nd, A[0]);
+        root->r = new node( nd, A[1]);
+      }
+      return root;
+    }else{
+      root = new node(d, A[mid]);
+    }
+    vector< pt> L, R;
+    cout << d << " " << A[mid].x[ d ] <<endl;
+    for( int i= l; i <=mid; i++) L.PB( A[i]);
+    for( int j= mid+1; j <=r; j++) R.PB( A[j]);
+    sort( L.begin(), L.end(), cmp( nd ));
+    sort( R.begin(), R.end(), cmp( nd ));
+    root->l = build( L, 0, L.size()-1, nd); 
+    root->r = build( R, 0, R.size()-1, nd);
     return root;
   }
 };
@@ -74,14 +94,15 @@ void bfs(node *root){
   q.push( root);
   while(!q.empty()){
     int s = q.size();
-    printf("level = %d, dim= %d\n", level, (level)%dim);
+    /* printf("level = %d, dim= %d\n", level, (level)%dim); */
     rep( i, 0, s){
       node *t = q.front(); q.pop();
       if( t->l == nullptr && t->r == nullptr){
-        t->p.show();
+        /* t->p.show(); */
       }else{
         int dx = t->p.x[t->d];
-        cout << "\t( > "<<dx<<")"<<endl<<endl;
+        cout << (level%dim) << " " << dx <<endl;
+        /* cout << "\t( > "<<dx<<")"<<endl<<endl; */
         if(t->l!=nullptr) q.push( t->l);
         if(t->r!=nullptr) q.push( t->r);
       }
@@ -96,12 +117,10 @@ int main(){
   #endif
   cin >> n >> dim;
   kdtree T( n );
-  sort( all( T.pts), cmp(0));
-  T.show();
 
   node *root;
-  root  = T.build( 0, n-1, 0 );
-  bfs( root );
+  root  = T.build( T.pts, 0, n-1, 0 );
+  /* bfs( root ); */
 
   
   return 0;
