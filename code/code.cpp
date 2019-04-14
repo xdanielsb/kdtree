8e172e929c4fcf0795a0920ea09913bba559985a
Daniel Santos <dfsantosbu@unal.edu.co>
2019-04-13 21:58:43 -0500

    Implement BFS to show the tree.

--- code/code.cpp
+++ code/code.cpp
@@ -4,7 +4,9 @@
 **/
 #include<bits/stdc++.h>
 #define debug( x ) cout << #x << " = " << x ;
+#define debug2( x, y) cout <<#x << " = " << x << " , " <<#y << " = " << y <<endl;
 #define rep( a,b, c) for(__typeof(b) a = b; a <(c); a++)
+
 #define all(x) x.begin(), x.end()
 using namespace std;
 using ll = long long;
@@ -22,7 +24,19 @@ struct pt{
     cout <<endl;
   }
 };
-
+struct node{
+  int d;
+  node *l, *r;
+  pt p;
+  node(){
+    r = l = nullptr;
+  }
+  node( int _d, pt &_p){
+    d = _d; 
+    p = _p;
+    r = l = nullptr;
+  }
+};
 struct cmp{
   int d;
   cmp( int _d): d(_d){};
@@ -30,7 +44,6 @@ struct cmp{
     return a.x[d] < b.x[d];
   }
 };
-
 struct kdtree{
   vector< pt > pts;
   int n;
@@ -40,12 +53,42 @@ struct kdtree{
   void show(){
     for( pt a: pts) a.show();
   }
-  void build(int l, int r, int d){
-    if( l > r ) return;
+  node* build(int l, int r, int d){
+    if( l >= r) {
+      return new node( d, pts[l] );
+    }
+    sort( pts.begin()+l, pts.begin()+r, cmp( d ));
     int mid = ( l + r )/2;
-    sort( all(T.pts), cmp(d));
+    node *root = new node(d, pts[mid]);
+    int nd = (d+1)%dim;
+    root->l = build( l, mid, nd);
+    root->r = build( mid+1, r, nd);
+    return root;
   }
 };
+
+void bfs(node *root){
+  queue < node* > q;
+  int level = 0;
+  if( root == nullptr) return;
+  q.push( root);
+  while(!q.empty()){
+    int s = q.size();
+    printf("level = %d, dim= %d\n", level, (level)%dim);
+    rep( i, 0, s){
+      node *t = q.front(); q.pop();
+      if( t->l == nullptr && t->r == nullptr){
+        t->p.show();
+      }else{
+        int dx = t->p.x[t->d];
+        cout << "\t( > "<<dx<<")"<<endl<<endl;
+        if(t->l!=nullptr) q.push( t->l);
+        if(t->r!=nullptr) q.push( t->r);
+      }
+    }
+    level++;
+  }
+}
 
 int main(){
   #ifdef LOCAL
@@ -53,8 +96,12 @@ int main(){
   #endif
   cin >> n >> dim;
   kdtree T( n );
-
+  sort( all( T.pts), cmp(0));
   T.show();
+
+  node *root;
+  root  = T.build( 0, n-1, 0 );
+  bfs( root );
 
   
   return 0;
