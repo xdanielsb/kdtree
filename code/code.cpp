c3d1f3d5b45017e34881501e2643d839399f6534
Daniel Santos <dfsantosbu@unal.edu.co>
2019-04-13 19:34:40 -0500

    Initial commit for kd-tree.

---
+++ code/code.cpp
@@ -0,0 +1,25 @@
+/**
+ *    author:  xdanielsb
+ *    created: 04.2019       
+**/
+#include<bits/stdc++.h>
+#define debug( x ) cout << #x << " = " << x << endl;
+using namespace std;
+using ll = long long;
+const int DIM = 10;
+
+struct pt{
+  ll x, y;
+  void show(){
+    debug(x); debug(y);
+  }
+}
+
+int main(){
+  ios::sync_with_stdio( false );
+  cin.tie( 0 );
+
+
+  
+  return 0;
+}
