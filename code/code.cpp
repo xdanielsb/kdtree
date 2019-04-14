b85611bc7b8da884233888f1650a9b86ea15a5fd
Daniel Santos <dfsantosbu@unal.edu.co>
2019-04-13 20:07:23 -0500

    Create base structure kd-tree.

--- code/code.cpp
+++ code/code.cpp
@@ -3,22 +3,43 @@
  *    created: 04.2019       
 **/
 #include<bits/stdc++.h>
-#define debug( x ) cout << #x << " = " << x << endl;
+#define debug( x ) cout << #x << " = " << x ;
+#define rep( a,b, c) for(__typeof(b) a = b; a <(c); a++)
 using namespace std;
 using ll = long long;
-const int DIM = 10;
+
+int n, dim;
+const int MAXD = 100;
 
 struct pt{
-  ll x, y;
+  ll x[ MAXD ];
   void show(){
-    debug(x); debug(y);
+    rep( i ,0, dim){
+      if( i ) cout << "\t"; debug( x[i] );
+    }
+    cout <<endl;
   }
-}
+};
+
+struct kdtree{
+  vector< pt > pts;
+  int n;
+  kdtree(int _n ): pts( _n ), n( _n ){
+    rep( i, 0, n) rep(j, 0, dim) cin >> pts[ i ].x[ j ];
+  }
+  void show(){
+    for( pt a: pts) a.show();
+  }
+};
 
 int main(){
-  ios::sync_with_stdio( false );
-  cin.tie( 0 );
+  #ifdef LOCAL
+    freopen("in", "r",stdin);
+  #endif
+  cin >> n >> dim;
+  kdtree T( n );
 
+  T.show();
 
   
   return 0;
