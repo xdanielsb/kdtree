caf4241c0c906ac95619922a34b83dc2f9e6d393
Daniel Santos <dfsantosbu@unal.edu.co>
2019-04-19 12:28:56 -0500

    Update run script, add restrictions.

--- code/run.sh
+++ code/run.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+      echo "Please write the number of points"
+      exit 0;
+fi
 python rnd.py "$1" > in
-g++ -DLOCAL -std=c++11 code.cpp && ./a.out < in  > lines
-python plot.py "in" "lines"
+g++ -DLOCAL -std=c++11 code.cpp && ./a.out < in
+python plot.py "in" "lines" &
+python digraph.py "nodes" "relations"
